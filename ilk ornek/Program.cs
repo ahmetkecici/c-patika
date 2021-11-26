69ee0bf26ce2e7147b02651ef13d768591349a2f
ahmetkecici 99 <ahmetkecici99@gmail.com>
2021-11-27 02:25:33 +0300

    C# giriş

---
+++ ilk ornek/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ilk_ornek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
